67411daac032b4bf1c00b18e9f11b90d2133e89f
Nezihe Sözen <nezihe.sozen@gmail.com>
2023-04-14 00:14:42 +0300

    Update graphnode.h

--- src/graphnode.h
+++ src/graphnode.h
@@ -12,18 +12,13 @@ class GraphEdge;
 class GraphNode
 {
 private:
-    //// STUDENT CODE
-    ////
 
     // data handles (owned)
-    std::vector<GraphEdge *> _childEdges;  // edges to subsequent nodes
+    std::vector<std::unique_ptr<GraphEdge>> _childEdges; // edges to subsequent nodes
 
     // data handles (not owned)
     std::vector<GraphEdge *> _parentEdges; // edges to preceding nodes 
-    ChatBot *_chatBot;
-
-    ////
-    //// EOF STUDENT CODE
+    ChatBot _chatBot;
 
     // proprietary members
     int _id;
@@ -44,17 +39,10 @@ public:
     // proprietary functions
     void AddToken(std::string token); // add answers to list
     void AddEdgeToParentNode(GraphEdge *edge);
-    void AddEdgeToChildNode(GraphEdge *edge);
-
-    //// STUDENT CODE
-    ////
+    void AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge);
 
     void MoveChatbotHere(ChatBot *chatbot);
-
-    ////
-    //// EOF STUDENT CODE
-
-    void MoveChatbotToNewNode(GraphNode *newNode);
+    void MoveChatbotToNewNode(GraphNode newNode);
 };
 
 #endif /* GRAPHNODE_H_ */
